b196ec33355adc1e25e1a7264b047657d1d420a8
Doug <dougdance@gmail.com>
2011-02-10 22:24:39 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParetoPublic::Application.config.secret_token = 'a651b7ecdadc8ac8736f9beaa68691c1fe505f3ca8faa95d0dbd57e8dd9b53fcca4d9e57c0935bbe203451bb090430c8d565982946a5390751002d07bd776908'
